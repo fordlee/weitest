eddbd607a4cf20f7726e58b03b59aa5fa7027ecb
fordlee <huli_yangthze@163.com>
2016-09-05 17:14:28 +0800

    修改dbbak

--- Admin/Lib/Action/UptimeRobotAction.class.php
+++ Admin/Lib/Action/UptimeRobotAction.class.php
@@ -4,17 +4,25 @@ class UptimeRobotAction extends Action {
     public function dbbak($token='') {
         if ($token === '98a8cbb2a66ddfca4db65a35eea43f65') {
             $hour = intval(date('H'));
-            if ($hour > 3) {
+            /*if ($hour > 3) {
                 exit('2');
             }
             $user = 'thehotgames';
             $password = 'weekmovie2013';
             $host = 'mysql.adonads.com';
             $dbname = 'mytests_co';
-            $filepath = str_replace('Runtime/Logs', 'dbbak', C('LOG_PATH'));
+            $filepath = str_replace('Runtime/Logs', 'dbbak', C('LOG_PATH'));*/
+            
+            $user = 'root';
+            $password = 'root';
+            $host = 'localhost';
+            $dbname = 'weitest';
+            $filepath = str_replace('Runtime\Logs', 'dbbak', C('LOG_PATH'));
+
             $file = $dbname . '_' . date('Y_m_d_H_i_s') . '.sql';
             $sqlcmd = "mysqldump --user=$user --password=$password --host=$host $dbname > $filepath$file";
             exec($sqlcmd);
+            //passthru($sqlcmd);
             exit('1');
         } else {
             exit('0');
